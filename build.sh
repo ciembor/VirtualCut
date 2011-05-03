1c9b27fa2e730f4df80c404dd0811739774a4ef4
ciembor <pub@ciemborowicz.pl>
2011-05-03 10:43:18 +0200

    scaffold and build/run scripts

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+scalac src/*.scala -d classes
---
+++ src/View.scala
@@ -0,0 +1,17 @@
+import swing._
+
+class View(model:Model) extends MainFrame {
+  
+    title = "VirtualCut"
+    
+    val menubar = new MenuBar
+    val menu = new Menu("Menu")
+    val menuItemExit = new MenuItem("Exit")
+    menuItemExit.tooltip = "Exit VirtualCut"
+    
+    //menu.menus ::= menuItemExit
+    menubar.menus ::= menu
+    
+    contents = {menubar}
+  
+}
