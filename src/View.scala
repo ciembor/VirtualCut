4cd0abed86bdd89eb265e6fb23bcca6e99327c91
ciembor <pub@ciemborowicz.pl>
2011-05-13 11:25:56 +0200

    Added BoxPanel.

--- src/View.scala
+++ src/View.scala
@@ -1,26 +1,46 @@
 import swing._
 
 class View(model:Model) extends MainFrame {
-  
-    title = "VirtualCut"
+  title = "app"
+   /* 
+  contents = new Panel() {
+    preferredSize = new Dimension(600,400)
+  }
     
-    contents = new Panel() {
-      preferredSize = new Dimension(600,400)
-    }
-    
-    val menuItemExit = new MenuItem("Exit")
-    menuItemExit.tooltip = "Exit VirtualCut"
+  val menuItemExit = new MenuItem("Exit")
+  menuItemExit.tooltip = "Exit"
     
 	menuBar = new MenuBar {   
-      contents += new Menu("Menu") {
-		val exitMenuItem = new MenuItem("Exit")
-		exitMenuItem.tooltip = "Exit VirtualCut"
-		
-        contents += exitMenuItem      
-        contents += new MenuItem(Action("Action item") { println(title) })       
-        contents += new Separator        
-        contents += new CheckMenuItem("Check me")
-      }
+    
+    contents += new Menu("Menu") {
+      val exitMenuItem = new MenuItem("Exit")
+      exitMenuItem.tooltip = "Exit"
+
+      contents += exitMenuItem      
+      contents += new MenuItem(Action("Action item") { println(title) })       
+      contents += new Separator        
+      contents += new CheckMenuItem("Check me")
     }
   
+  }
+  */
+  
+  contents = new BoxPanel(Orientation.Vertical) {
+  
+    val parameters = new FlowPanel() {
+      contents += new Label("Tempo: ")
+      contents += new ComboBox(Seq("80", "100", "120", "140"))
+      contents += new Label("Metric: ")
+      contents += new Label("Note: ")
+    }
+        
+    val controls = new FlowPanel() {
+      contents += new Button( "klop" ) 
+    }
+    
+    contents += parameters
+    contents += controls
+    
+  }
+
 }
